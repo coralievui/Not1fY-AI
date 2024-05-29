5e99767309ec6d8177fffd8fdeace77aa4b7321f
Vuillemin, Coralie <coralie.vuillemin@accenture.com>
2024-05-29 14:06:43 +0200

    MAJ MAy 24

---
+++ child-theme/functions.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * https://developers.elementor.com/docs/hello-elementor-theme/
+ *
+ * @package HelloElementorChild
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+add_filter( 'jetpack_sitemap_image_skip_post', '__return_true' );
+
+add_filter( 'xmlrpc_enabled', '__return_false' );
+add_filter( 'wp_xmlrpc_server_class', function( $class ) {
+  http_response_code( 403 );
+  exit;
+} );
